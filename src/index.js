209a75589b1b15bc9369d83da7e9c374bb4d1b62
tuan-cpu <tuan.lq184323@sis.hust.edu.vn>
2022-05-19 09:30:50 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
 import { registerServiceWorker } from './serviceWorker'
 
 import 'common/firebase'
@@ -13,8 +12,4 @@ ReactDOM.render(
   document.getElementById('root'),
 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
 registerServiceWorker()
